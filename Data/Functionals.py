7fbbcc6374836e578d14a3f3c9f10eea32d8da76
Edson <edson@kognitus.com.br>
2019-08-31 20:44:00 -0300

    Adicionei algumas funcoes para somar dicionarios

--- Data/Functionals.py
+++ Data/Functionals.py
@@ -1,3 +1,29 @@
 import numpy as np
 
-def phi( gzmodel,  ):
+def phi( gzmodel, gzcalc ):
+    res = [ ]
+    for gz in gzcalc:
+        res.append( ( np.linalg.norm( gzmodel - gz ) )**2 )
+
+    return  res
+
+def somadict( dict1, dict2 ):
+    soma = [ ]
+    temp = [ ]
+
+    for index1, key1 in enumerate( dict1 ):
+        for index2, key2 in enumerate( dict2 ):
+            temp.append( dict2[ key2 ] )
+
+        soma.append( np.array( dict1[ key1 ] + temp[ index1 ] ) )
+        temp = [ ]
+
+    return np.array( soma )
+
+
+def dictTimesConstant( dict, constant ):
+
+    for key in dict:
+        dict[ key ] = constant*dict[ key ]
+
+    return dict
