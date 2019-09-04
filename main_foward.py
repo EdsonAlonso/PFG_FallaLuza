21f413aaf78d3a77fa6d360cbf68d7b215033452
Edson <edson@kognitus.com.br>
2019-09-03 22:33:26 -0300

    Minor changes

--- main_foward.py
+++ main_foward.py
@@ -3,7 +3,7 @@ import numpy as np
 from Data.Population import Fontes
 import matplotlib.pyplot as plt
 from Models import rect, sphere
-from modules.auxiliar import sigmoide
+from modules.auxiliar import sigmoide, softmax
 
 def alp(x, y):
     fun = -(x * y) ** (0.5) * np.sin(x) * np.sin(y)
@@ -14,41 +14,48 @@ def phi( x,y ):
 
 
 if __name__ == "__main__":
-    xobs = np.linspace( 0,10,300 )
+    xobs = np.linspace( -10,10,300 )
     zobs = np.zeros( len( xobs ) )
-    xmin, xmax = 0.0, 10.0
-    ymin, ymax = 0.5, 10.0
-    min_bounds = [xmin, ymin]
-    max_bounds = [xmax, ymax]
-    npop = 200
+    xmin, xmax = -0.5, 0.5
+    ymin, ymax =  2.0, 6.0
+    min_bounds = [xmin, ymin, 5e4]
+    max_bounds = [xmax, ymax, 5e4]
+    npop = 500
     pmut = 0.1
     ngera = int( 500 )
     npar = len( min_bounds )
 
-    model = rect( 4.5, 3.0, 5.0 , 4.0 , 3e8)
+    model = rect( -0.5,0.5,2.0,6.0,5e5 )
     model_gz = model.Gz( xobs, zobs )
     model_gz_noised = model.addnoise( )
 
     massbounds = [ 1e4,1e7 ]
     pop = Fontes( )
-    pop.Gera( min_bounds, max_bounds, massbounds, nfontes = npop , nind = 4)
+    pop.Gera( min_bounds, max_bounds, nfontes = npop , nind = 1)
     fontes = pop.asArray( )
     gz = pop.Gz( xobs, zobs )
 
 
-    for index,ind in enumerate(pop.fontes):
-        plt.figure()
-        plt.title(f'Individuo {index}')
-        plt.subplot(211)
-        plt.plot(xobs, gz[index], label = 'Gz bolinhas')
-        plt.plot(xobs, model_gz_noised, label = 'Gz observado + ruído')
-        plt.grid( )
-        plt.legend( )
-        plt.subplot(212)
-        for fonte in ind:
-            plt.scatter( fonte.params[0], fonte.params[1], c = 'black' )
-        plt.xlim(min(xobs), max(xobs) )
-        plt.gca().invert_yaxis()
-        plt.grid( )
-        #plt.savefig('Imagegm'+str(index)+'.png')
-    plt.show( )
+    plt.figure()
+    plt.subplot(211)
+    plt.plot(xobs, model_gz, label = str( ( phi( model_gz, gz ) ) ) )
+    plt.plot( xobs, gz[0])
+    plt.grid( )
+    plt.xlim(-10,10)
+    plt.legend( )
+
+    plt.subplot(212)
+    plt.plot( [ model.params[ 0 ], model.params[ 0 ], model.params[ 1 ], model.params[ 1 ], model.params[ 0 ] ],\
+              [ model.params[ 2 ],model.params[ 3 ], model.params[ 3 ], model.params[ 2 ],model.params[ 2 ] ],".-" )
+    for key in pop.fontes[ 0 ]:
+        plt.scatter( key.params[ 0 ], key.params[ 1 ] )
+    plt.grid( )
+    plt.ylim( 0,10 )
+    plt.gca( ).invert_yaxis( )
+    plt.xlim( -10,10 )
+    # plt.show( )
+
+    plt.figure()
+    plt.plot(xobs, softmax(xobs))
+    plt.show()
+
