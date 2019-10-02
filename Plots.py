1b8638b9787fdb125f34daa28f759f09fc9f8c01
Edson <edson@kognitus.com.br>
2019-10-02 12:32:54 -0300

    Adicionei o segundo modelo

--- Plots.py
+++ Plots.py
@@ -3,21 +3,27 @@ import numpy as np
 
 def plotRect( rect , Gz, xobs, zlim, fill = False ):
 
-    model = rect
+    models = rect
 
     plt.subplot(211)
-    plt.plot(xobs, Gz)
+    Gz_model = 0
+    for gz in Gz:
+        Gz_model += gz
+
+    plt.plot(xobs, Gz_model,'b' )
     plt.xlim(min(xobs), max(xobs))
     plt.grid()
 
     plt.subplot(212)
-    plt.plot([model.params[0], model.params[0], model.params[1], model.params[1], model.params[0]], \
-             [model.params[2], model.params[3], model.params[3], model.params[2], model.params[2]], ".-")
-    plt.grid()
-    if fill == True:
-        plt.fill_between([model.params[0], model.params[0], model.params[1], model.params[1], model.params[0]], \
-                         [model.params[2], model.params[3], model.params[3], model.params[2], model.params[2]])
+    for model in models:
+        plt.plot([model.params[0], model.params[0], model.params[1], model.params[1], model.params[0]], \
+                 [model.params[2], model.params[3], model.params[3], model.params[2], model.params[2]], "-b")
 
+        if fill == True:
+            plt.fill_between([model.params[0], model.params[0], model.params[1], model.params[1], model.params[0]], \
+                             [model.params[2], model.params[3], model.params[3], model.params[2], model.params[2]], alpha = 0.3, facecolor = 'blue')
+
+    plt.grid()
     plt.ylim(0 , 1000 )
     plt.xlim(min(xobs), max(xobs))
     plt.gca().invert_yaxis()
